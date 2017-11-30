449bfc28adc7a360bbeb66847092d356587df800
vivek mishra <vm368@afsconnect1.njit.edu>
2017-11-29 23:50:13 -0500

    database file

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+/*define('DATABASE', 'vm368');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('CONNECTION', 'localhost');*/
+define('DATABASE', 'vm368');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('CONNECTION', 'localhost');
+?>
