066a2a89f912337ca218626903de68376390a1e3
vivek mishra <vm368@afsconnect1.njit.edu>
2017-12-05 22:35:23 -0500

    collections with files

---
+++ collections/accounts.php
@@ -0,0 +1,25 @@
+<?php
+
+class accounts extends \database\collection
+{
+    protected static $modelName = 'account';
+
+    //This is the function to write to find user by ID for login.
+    //Don't forget to return the object see findOne in the collection class
+    public static  function findUserbyUsername($uname) {
+		$tableName=get_called_class();
+		$sql= 'SELECT *FROM'.$tableName.'WHERE email="' . $uname.'"';
+		echo $sql;
+		$recordsSet= self::getResults($sql); // return one object 
+		return $recordsSet[0];
+	}
+	public static function checkPassword($pass1,$pass2){
+		if ($pass1 == $pass2){
+			return 1;
+	}else {
+		return 0;
+	}
+}
+}
+
+?>
