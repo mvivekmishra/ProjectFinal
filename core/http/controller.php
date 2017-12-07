bbd8c3e2202167c5399b49fa8d01e335e4f129e2
vivek mishra <vm368@afsconnect2.njit.edu>
2017-12-07 12:52:47 -0500

    more changes

--- core/http/controller.php
+++ core/http/controller.php
@@ -5,7 +5,10 @@ namespace http;
 class controller
 {
 
-
+/*public function __construct()
+{
+	session_start();
+}*/
 //this gets the HTML template for the application and accepts the model.  The model array can be used in the template
     static public function getTemplate($template, $data = NULL)
     {
