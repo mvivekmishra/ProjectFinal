19c7bd86c8eb716c7ae0e049a698782152aafd9f
vivek mishra <vm368@afsconnect2.njit.edu>
2017-12-07 14:42:09 -0500

    new login and sign up

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,11 +4,18 @@
 <head>
     <meta charset="utf-8">
 
-    <title>WSD Project TODO</title>
+    <title>Log-IN</title>
     <meta name="description" content="WSD Project">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
+	
+ 
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<script src="bootstrap/js/jquery-1.12.2.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="bootstrap/css/datepicker.css">
+	<script src="bootstrap/js/bootstrap-datepicker.js"></script>
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -28,24 +35,40 @@
 
 <!--<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
 <h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>-->
+		<nav class="navbar navbar-inverse">
+	
+		<div class="container-fluid">
+		
+			
+			<div class="navbar-header">
+				<a hre="#" class="navbar-brand" />TODO Project </a>
+				
+			</div></nav>
+			
+         
+			<div align="center" class="mylogin">
+					<br/>
+				
+			<h2> <strong>Log-In </strong></h2>
 
 <form action="index.php?page=accounts&action=login" method="POST">
 
-    <div class="container">
+    <div align= "center" class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text"  placeholder="Enter Username" name="uname" required></br></br>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password"  placeholder="Enter Password" name="psw" required></br></br>
 
-        <button type="submit">Login</button>
+        <button type="submit">Login</button></br>
     </div>
 
-
+</br>
 </form>
+
 <form action= "index.php?page=accounts&action=signup" method ="POST">
-<div class= "container">
-<label><b> Not Registered !</b></label><button type ="submit">Signup</button>
+<div align ="center" class= "container">
+<label><b></b></label><button type ="submit">Signup</button>
 </div>
 </form>
 
--- pages/new_user.php
+++ pages/new_user.php
@@ -2,9 +2,22 @@
 
 <html lang="en">
 <head>
+    <head>
     <meta charset="utf-8">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<script src="bootstrap/js/jquery-1.12.2.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+		<!-- Date time picker attachement -->
+		<link rel="stylesheet" href="bootstrap/css/datepicker.css">
+
+	<!--	<script src="bootstrap/js/jquery-1.9.1.min.js"></script> -->
+
+		<script src="bootstrap/js/bootstrap-datepicker.js"></script>
+
+		<title> Sign-Up </title>
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -17,11 +30,40 @@
 //this is how you print something
 //print utility\htmlTable::genarateTableFromMultiArray($data);
 ?>
+</br>
+<nav class="navbar navbar-inverse">
+	
+		<div class="container-fluid">
+		
+			<!-- Logo -->
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" /> TODO Project </a>
+			</div>
+	
+			<!-- Menu on the right -->
+			
+				<ul class="nav navbar-nav navbar-right">
+					
+					<li> <a href="index.php"><span class="glyphicon glyphicon-log-in"> Log-In </a> </li>
+				
+				</ul>
+			
+			</div>
+			
+		</div>
+		
+	</nav>
+			
 
-<form action="index.php?page=accounts&action=register" method="POST">
+<form action="index.php?page=accounts&action=register" method="POST">   
+         
+		
 
-    <div class="container">
-        
+           <div align= "center" class="container-fluid">
+	
+            <div class="form" align="center">
+			<h2> <strong> Sign-Up </strong> </h2>
+			
 		<label><b>Email</b></label>
         <input type="text" placeholder="Enter Email" name="email" required></br></br>
 		
@@ -45,7 +87,7 @@
         <label><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="password" required></br></br>
 
-        <button type="submit">Register</button><a href ="index.php">Cancel</a></br></br>
+        <button type="submit">Register</button>&nbsp;&nbsp;<a href ="index.php">Cancel</a></br></br>
 
     </div>
 
