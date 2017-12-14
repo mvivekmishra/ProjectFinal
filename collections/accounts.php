480477bc2d025728e9d4e3483925f482a96f5394
vivek mishra <vm368@afsconnect2.njit.edu>
2017-12-14 11:29:43 -0500

    few updates

--- collections/accounts.php
+++ collections/accounts.php
@@ -1,34 +1,21 @@
 <?php
-
 class accounts extends \database\collection
 {
     protected static $modelName = 'account';
-
     //This is the function to write to find user by ID for login.
     //Don't forget to return the object see findOne in the collection class
     public static  function findUserbyUsername($uname) {
-		$tableName=get_called_class();
-		$sql= 'SELECT *FROM ' .$tableName. ' WHERE email="' . $uname.'"';
-		echo $sql;
-		$recordsSet= self::getResults($sql); // return one object ($sql,email)
-		echo $recordsSet; // additional print 
-		if (is_null($recordsSet)){
-			return FALSE;
-		}else
-		
-		{ return $recordsSet[0];}
-		//return $recordsSet[0];
-	}
-	/*public static function checkPassword($pass1,$pass2){
-		if ($pass1 == $pass2){
-			return 1;
-	}else {
-		return 0;
-	}*/
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE email ="' . $uname.'"';
+        echo $sql;
+        //grab the only record for find one and return as an object
+        $recordsSet = self::getResults($sql);
+        if (is_null($recordsSet)) {
+            return FALSE;
+        } else {
+            return $recordsSet[0];
+        }
+        //return $recordsSet[0];
+    }
 }
-// public static function findUserbyEmail($email)
-//$tableName=get_called_class();
-
-
-
 ?>
