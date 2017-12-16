e1f36f50b74eaedea0be7bc073739dcc5ad4c76e
vivek mishra <vm368@afsconnect2.njit.edu>
2017-12-15 23:51:08 -0500

    all updates done

--- collections/accounts.php
+++ collections/accounts.php
@@ -1,21 +1,25 @@
+
+
+
+
 <?php
-class accounts extends \database\collection
+class accounts extends \database\collection 
 {
     protected static $modelName = 'account';
-    //This is the function to write to find user by ID for login.
-    //Don't forget to return the object see findOne in the collection class
+    
+	//This is the function to write to find user by ID for login.
+    
     public static  function findUserbyUsername($uname) {
         $tableName = get_called_class();
         $sql = 'SELECT * FROM ' . $tableName . ' WHERE email ="' . $uname.'"';
-        echo $sql;
-        //grab the only record for find one and return as an object
+        // You may print sql command for display and correctness of SQl syntax
         $recordsSet = self::getResults($sql);
         if (is_null($recordsSet)) {
             return FALSE;
         } else {
             return $recordsSet[0];
         }
-        //return $recordsSet[0];
+        
     }
 }
 ?>
